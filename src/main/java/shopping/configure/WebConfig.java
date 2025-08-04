63a1ddc0a1e1ad9b8d258f684dea411da932fb30
Hoàng Tài <118200491+tai1112004@users.noreply.github.com>
2025-08-05 01:27:28 +0700

    fix cors

--- src/main/java/shopping/configure/WebConfig.java
+++ src/main/java/shopping/configure/WebConfig.java
@@ -11,10 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("*")  // Thay đổi này
+            .allowedOriginPatterns("*")  
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
-            .allowCredentials(false)     // Và thay đổi này
+            .allowCredentials(false)     
             .maxAge(3600);
     }
 }
