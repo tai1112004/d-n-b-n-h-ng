2245f47b3d5cdc50ac9a19aad63ee3150f3b83d8
Hoàng Tài <118200491+tai1112004@users.noreply.github.com>
2025-08-05 01:19:11 +0700

    fix cors

--- src/main/java/shopping/API/AuthenticationAPI.java
+++ src/main/java/shopping/API/AuthenticationAPI.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import shopping.Service.Authentication;
 import shopping.model.resultUser;
 import shopping.request.UserRequest;
-
+@CrossOrigin(origins = "*")
 @RestController
 public class AuthenticationAPI {
 	@Autowired
